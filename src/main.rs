a4f50fb2a682906e8499ea94a558df6ffe63cc7c
Matt Baker <matt.baker@airbnb.com>
2016-09-01 17:16:57 -0700

    Don't send byte by byte and don't drop bytes

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,58 @@
 use std::thread;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{channel, Sender, Receiver};
 use std::io::{self, Read, Write};
 
+enum WriteError {
+    EOF,
+    HungUp,
+}
+
 fn main() {
-    let (transmitter, receiver) = channel::<u8>();
+    let (transmitter, receiver) = channel::<[u8; 512]>();
 
     thread::spawn(move|| {
-        for byte in io::stdin().bytes() {
-            // TODO: r u freaking kidding me with these unwraps
-            // don't send a byte if the byte read fails duh
-            transmitter.send(byte.unwrap()).unwrap();
-        }
+        read_stdin(&transmitter);
     });
 
-    let mut dead = false;
-    while !dead {
-        let byte = receiver.recv();
+    // This function "fails" when it's done; w/e
+    let _ = write_stdout(&receiver);
+}
+
+fn read_stdin(transmitter: &Sender<[u8; 512]>) {
+    let mut bytes: [u8; 512] = [0; 512];
+    let mut index = 0;
+
+    for byte in io::stdin().bytes() {
         match byte {
             Ok(data) => {
-                // TODO: buffer data when write fails
-                io::stdout().write(&[data]);
+                if index == 512 {
+                    let transfer = bytes;
+                    bytes = [0; 512];
+
+                    transmitter.send(transfer).unwrap();
+                    index = 0;
+                }
+
+                bytes[index] = data;
+                index = index + 1;
             },
-            Err(_) => {
-                dead = true;
-            },
+
+            // Ignore impossible bytes
+            Err(_) => (),
         }
+    }
+
+    if index > 0 {
+        transmitter.send(bytes).unwrap();
+    }
+}
+
+fn write_stdout(receiver: &Receiver<[u8; 512]>) -> Result<(), WriteError> {
+    loop {
+        // If this fails, stdin hung up; no more data to write
+        let bytes = try!(receiver.recv().map_err(|_| WriteError::EOF));
+
+        // If this fails, stdout hung up; nothing is listening anymore
+        try!(io::stdout().write_all(&bytes).map_err(|_| WriteError::HungUp));
     }
 }
